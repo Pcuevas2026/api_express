6eee0fcc7e2415e1ed6f8e6c3c66d4723b4392f0
Pcuevas2026 <pdcuevasp@correo.url.edu.gt>
2025-09-27 16:09:00 -0600

    Se agrega completadas a endpoint get de nota

--- routes/notaRoutes.js
+++ routes/notaRoutes.js
@@ -8,11 +8,26 @@ router.get('/alumnos/:prueba', async (req, res, next) => {
       `SELECT 
          n.id_alumno,
          replace(concat(al.primer_nombre, ' ',  al.segundo_nombre, ' ', al.primer_apellido, ' ', al.segundo_apellido),'  ',' ') as nombre_alumno,
-         n.nota
+         n.nota,
+         IFNULL(res.completadas,0) as completadas
        FROM nota n
        INNER JOIN alumno al ON al.id_alumno = n.id_alumno
+       LEFT JOIN(
+        SELECT
+          id_alumno
+          ,COUNT(1) as completadas
+        FROM respuesta
+        WHERE id_pregunta IN(
+          SELECT id_pregunta
+          FROM pregunta
+          WHERE id_prueba = ?
+        )
+        and respuesta IS NOT NULL
+        GROUP BY id_alumno
+      ) as res
+        ON res.id_alumno = n.id_alumno
        WHERE n.id_prueba = ?`,
-      [req.params.prueba]
+      [req.params.prueba, req.params.prueba]
     );
     
     res.json(alumnos);
